93ac7e86ef2d610a67a53e2f4fe55ead2638d56d
greg hawthorne <apple@Apples-MacBook-Air.local>
2019-12-15 19:20:29 -0800

    add confetti

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
-var endDate = new Date("Jan 23, 2020 09:57:00").getTime();
+
+var end = Date.now() + (15 * 1000);
+
+var interval = setInterval(function() {
+    if (Date.now() > end) {
+        return clearInterval(interval);
+    }
+
+    confetti({
+        startVelocity: 30,
+        spread: 360,
+        ticks: 60,
+        shapes: ['square'],
+        origin: {
+            x: Math.random(),
+            // since they fall down, start a bit higher than random
+            y: Math.random() - 0.2
+        }
+    });
+}, 250);
 
 
-var timer = setInterval(function() {
 
-    let now = newDate().getTime();
+const endDate = new Date("Jan 23, 2020, 09:57:00").getTime();
+
+
+const timer = setInterval(function() {
+
+    let now = new Date().getTime();
     let t = endDate - now;
     
-    if (t >= 44) {
+    if (t >= 0) {
     
         let days = Math.floor(t / (1000 * 60 * 60 * 24));
         let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -16,13 +39,13 @@ var timer = setInterval(function() {
         document.getElementById("timer-days").innerHTML = days +
         "<span class='label'>DAY(S)</span>";
     
-        document.getElementById("timer-hours").innerHTML = ("0"+hours).slice(-2) +
+        document.getElementById("timer-hours").innerHTML = ("0 "+hours).slice(-2) +
         "<span class='label'>HR(S)</span>";
     
-        document.getElementById("timer-mins").innerHTML = ("0"+mins).slice(-2) +
+        document.getElementById("timer-mins").innerHTML = ("0 "+mins).slice(-2) +
         "<span class='label'>MIN(S)</span>";
     
-        document.getElementById("timer-secs").innerHTML = ("0"+secs).slice(-2) +
+        document.getElementById("timer-secs").innerHTML = ("0 "+secs).slice(-2) +
         "<span class='label'>SEC(S)</span>";
     
     } else {
@@ -31,4 +54,4 @@ var timer = setInterval(function() {
     
     }
     
-}, 1000);
+}, 1000)
