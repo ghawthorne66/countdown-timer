81b062e0fafb8017c8ee3a129e14452a7d81d013
Greg Hawthorne <ghawthorne@me.com>
2019-12-24 16:26:26 -0800

    add css for christmas theme

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
 
-var end = Date.now() + (15 * 1000);
+// var end = Date.now() + (15 * 1000);
 
-var interval = setInterval(function() {
-    if (Date.now() > end) {
-        return clearInterval(interval);
-    }
+// var interval = setInterval(function() {
+//     if (Date.now() > end) {
+//         return clearInterval(interval);
+//     }
 
+//     confetti({
+//         startVelocity: 30,
+//         spread: 360,
+//         ticks: 60,
+//         shapes: ['square'],
+//         origin: {
+//             x: Math.random(),
+//             // since they fall down, start a bit higher than random
+//             y: Math.random() - 0.2
+//         }
+//     });
+// }, 250);
+
+var end = Date.now() + (15 * 2000);
+(function frame() {
     confetti({
-        startVelocity: 30,
-        spread: 360,
-        ticks: 60,
-        shapes: ['square'],
+        particleCount: 1,
+        startVelocity: 0,
+        ticks: 20,
         origin: {
             x: Math.random(),
             // since they fall down, start a bit higher than random
             y: Math.random() - 0.2
-        }
+        },
+        colors: ['#ffffff'],
+        shapes: ['circle']
     });
-}, 250);
+
+    if (Date.now() < end) {
+        requestAnimationFrame(frame);
+    }
+}());
 
 
 
